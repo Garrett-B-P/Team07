3455c6be5762b4a4e6859543ecb1bbd3d434fa33
B33Mack <braedenmcmurray@gmail.com>
2021-02-16 15:49:06 -0700

    Testing from Braeden

--- app/src/main/java/com/example/team07/MainActivity.java
+++ app/src/main/java/com/example/team07/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+// Just a comment from Braeden to test if we gotz it workin
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
