354b61d49ae82b88be75ae201e818a2ec3403127
Garrett-B-P <77997734+Garrett-B-P@users.noreply.github.com>
2021-02-16 16:56:21 -0600

    test

--- app/src/main/java/com/example/team07/MainActivity.java
+++ app/src/main/java/com/example/team07/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 // Just a comment from Braeden to test if we gotz it workin
+// Comment from Garrett
 
 public class MainActivity extends AppCompatActivity {
 
