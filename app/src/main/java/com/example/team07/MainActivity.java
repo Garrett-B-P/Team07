78fc4805047669492e2bf2f22531cf19e4533e3c
Cajsa S <zaelafox@hotmail.com>
2021-02-16 16:05:59 -0800

    Comment from Cajsa

--- app/src/main/java/com/example/team07/MainActivity.java
+++ app/src/main/java/com/example/team07/MainActivity.java
@@ -4,8 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-// Just a comment from Braeden to test if we gotz it workin
+// Just a comment from Braeden to test if we got it working
 // Comment from Garrett
+// Fixed the grammar because Cajsa's a grammar nazi. Sometimes.
 
 public class MainActivity extends AppCompatActivity {
 
