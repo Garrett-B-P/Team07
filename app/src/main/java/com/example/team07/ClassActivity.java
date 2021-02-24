5d16fa4b7d2e9d2294b3e5f748d35ea257bf068b
B33Mack <braedenmcmurray@gmail.com>
2021-02-24 16:22:58 -0700

    Function Stubs for Class Activity, one currently working.

--- app/src/main/java/com/example/team07/ClassActivity.java
+++ app/src/main/java/com/example/team07/ClassActivity.java
@@ -4,11 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClassActivity extends AppCompatActivity {
+
+    String title;
+    List notes = new ArrayList<NotesActivity>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class);
+
+        title = "unchangedTitle";
+    }
+
+    private void addNote(){
+
+    }
+
+    private void deleteNote(){
+
+    }
+
+    private void editNote(){
+
+    }
+
+    public void sortNotes(){
+
+    }
+
+    public String getClassTitle(){
+        return title;
+    }
+
+    public List searchClassNotes(){
+
     }
 }
