4c4725e3713e738fa008af48cb5356807796ff1e
Cajsa S <zaelafox@hotmail.com>
2021-02-24 17:15:10 -0800

    Added EditText import to ClassActivity and NotesActivity

--- app/src/main/java/com/example/team07/NotesActivity.java
+++ app/src/main/java/com/example/team07/NotesActivity.java
@@ -2,6 +2,7 @@ package com.example.team07;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.view.View;
 
 import java.util.ArrayList;
