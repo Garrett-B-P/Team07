5000bd563270e82ed17f38026cddce96942516fa
B33Mack <braedenmcmurray@gmail.com>
2021-02-24 17:48:33 -0700

    Began class.xml and changed notes title and body to EditText objects.

--- app/src/main/java/com/example/team07/ClassActivity.java
+++ app/src/main/java/com/example/team07/ClassActivity.java
@@ -4,21 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.view.View;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ClassActivity extends AppCompatActivity {
 
-    String classTitle;
+    EditText classTitle = findViewById(R.id.classTitle);
     List notes = new ArrayList<NotesActivity>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class);
-
-        classTitle = "unchangedTitle";
     }
 
     private void addNote(){
@@ -38,7 +37,7 @@ public class ClassActivity extends AppCompatActivity {
     }
 
     public String getClassTitle(){
-        return classTitle;
+        return classTitle.getText().toString();
     }
 
     public List searchClassNotes(){
